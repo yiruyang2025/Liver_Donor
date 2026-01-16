4c153a60d1dbb9b8c81b88024f7fc7655512ab74
YiruYang <162698398+yiruyang2025@users.noreply.github.com>
2026-01-16 04:09:33 +0100

    Update ssl_objectives.py

--- src/ssl_objectives.py
+++ src/ssl_objectives.py
@@ -2,6 +2,7 @@
 
 import torch
 import torch.nn.functional as F
+
 def nt_xent_loss(z_i,z_j,temperature=0.07):
  batch_size=z_i.shape[0]
  z_i=F.normalize(z_i,dim=1)
@@ -20,14 +21,17 @@ def nt_xent_loss(z_i,z_j,temperature=0.07):
  labels=torch.zeros(2*batch_size,dtype=torch.long,device=z.device)
  loss=F.cross_entropy(logits,labels)
  return loss
+
 def masking_loss(x_original,x_reconstructed,mask):
  masked_original=x_original*mask
  masked_reconstructed=x_reconstructed*mask
  loss=F.mse_loss(masked_original,masked_reconstructed)
  return loss
 class MaskingPretrainingObjective:
+
  def __init__(self,mask_ratio=0.3):
   self.mask_ratio=mask_ratio
+
  def __call__(self,x,model):
   batch_size=x.shape[0]
   feature_dim=x.shape[1]
@@ -37,9 +41,11 @@ class MaskingPretrainingObjective:
   loss=masking_loss(x,x_reconstructed,mask)
   return loss
 class ContrastivePretrainingObjective:
+
  def __init__(self,temperature=0.07,augmentation_fn=None):
   self.temperature=temperature
   self.augmentation_fn=augmentation_fn
+
  def __call__(self,x,model):
   if self.augmentation_fn is None:
    x_i,x_j=self._default_augment(x)
@@ -49,17 +55,20 @@ class ContrastivePretrainingObjective:
   z_j=model(x_j)
   loss=nt_xent_loss(z_i,z_j,temperature=self.temperature)
   return loss
+
  def _default_augment(self,x):
   noise=torch.randn_like(x)*0.1
   x_i=x+noise
   x_j=x+torch.randn_like(x)*0.1
   return x_i,x_j
+
 class HybridObjective:
  def __init__(self,contrastive_weight=0.5,masking_weight=0.5,temperature=0.07,mask_ratio=0.3):
   self.contrastive_objective=ContrastivePretrainingObjective(temperature)
   self.masking_objective=MaskingPretrainingObjective(mask_ratio)
   self.contrastive_weight=contrastive_weight
   self.masking_weight=masking_weight
+
  def __call__(self,x,model):
   loss_contrastive=self.contrastive_objective(x,model)
   loss_masking=self.masking_objective(x,model)
